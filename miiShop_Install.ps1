82767ede30b470e39b245e1edfb360a6fdb8954d
Engarak <engarak@tutanota.com>
2019-11-26 21:00:43 -0500

    Updated installer to use 0.2.9 release
    Title

--- miiShop_Install.ps1
+++ miiShop_Install.ps1
@@ -7,12 +7,12 @@ function get-latestmiiShop ([String] $question,[String] $installType)
     $repo = "Engarak/MiiShop"
     if($installType.ToUpper() -eq 'UPGRADE')
     {        
-        $file = "miiShop_upgrade-0_2_x-0_2_7.zip"        
+        $file = "miiShop_upgrade-0_2_x-0_2_9.zip"        
         $uoriDir = Get-Folder -displayMesage 'Where are the .cia files located?'
     }
     else
     {
-        $file = "miiShop_install-0_2_7.zip"
+        $file = "miiShop_install-0_2_9.zip"
         $uoriDir = Get-Folder -displayMesage 'Where is the miiShop.ps1 file located?'
     }
 
@@ -46,7 +46,7 @@ function get-latestmiiShop ([String] $question,[String] $installType)
     Remove-Item $zip -Force
     Remove-Item $folder -Recurse -Force
     $filePath="$uoriDir\start.bat" 
-    cd $uoriDir
+    set-location $uoriDir
     #$file = Get-ChildItem "$uoriDir\database\settings.csv" #for future upgrades force the settings file to get updated to force a rebuild
     #$file.LastWriteTime = Get-Date
     Write-Output 'Upgrade/install completed, launching miiShop'
@@ -56,7 +56,6 @@ function get-latestmiiShop ([String] $question,[String] $installType)
 
 Function Get-Folder ([string]$displayMesage)
 {
-    $initialDirectory=$env:HOMEDRIVE
     [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")|Out-Null
     $foldername = New-Object System.Windows.Forms.FolderBrowserDialog
     $foldername.Description = $displayMesage
@@ -98,34 +97,3 @@ Switch ($result)
      }
 
 Stop-Transcript
-# SIG # Begin signature block
-# MIIFYQYJKoZIhvcNAQcCoIIFUjCCBU4CAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
-# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQURzw2TE2+YBmXy4IbGGIkBrua
-# lt+gggMGMIIDAjCCAeqgAwIBAgIQXvibBfOpFbJJxUJ9WcAiEDANBgkqhkiG9w0B
-# AQsFADAQMQ4wDAYDVQQDDAVUSVRBTjAeFw0xOTA0MjAwNDAyMzNaFw0yMDA0MjAw
-# NDIyMzNaMBAxDjAMBgNVBAMMBVRJVEFOMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8A
-# MIIBCgKCAQEAsJolne0jdB/Rt4/hG0HPh7wbI4RRKdB7T3jsNFjyRD/9pre1OP+w
-# JbhsBEnfcQOOu2AGJPO8prqgA+JgCIdw/2CM2NhLQ/dmtMDwRkLCnzdrLd42Yj/U
-# rPArWBqpJy+4EAw6ueGUBzuSSzPgqWM9fDCIvxCqAGMIEQHDCM3E268ngUl/KOai
-# UgQ7vjOWeI8G3Akyj4mnCYBjF5Y/W8D7dlgPGrRhFfxxXBL2ky/3R+42sQl7UH8s
-# EXOeEGHI3pYYkIm42EnDOuNx1F03x9CA6uHMo3y7lKAF3QmM/VIWg7kUdPnN8lWL
-# 4OfeKaPj3kNq7AHk+J2sBGa5ktbXSG/OTQIDAQABo1gwVjAOBgNVHQ8BAf8EBAMC
-# B4AwEwYDVR0lBAwwCgYIKwYBBQUHAwMwEAYDVR0RBAkwB4IFVElUQU4wHQYDVR0O
-# BBYEFCXPxQZnoT5Wu2iA/s9nMzhBeowMMA0GCSqGSIb3DQEBCwUAA4IBAQA7l9cT
-# mtLhRHqL9ahxLZEyQQcPyfkC9xC5/RdNpaJM/w0nT4KA2OJmOovl1+5aCcs3hO/l
-# eAfNmpLoTF6MShCnCKQRsytlJqgFTyI7lTZp3pWowMaPuodN3jI3UdtvMR4O0tIm
-# yyuvKxHcFD+U5/E20zHZIu4M6hwqMeiAGR8+LOHykGAOGTr1e7VpOW703YF9ht70
-# jTtbgbLl4iiMWXLRZScrhoj/j3nKjGoV/9/m72LEhqTKc9EPraLqYLloZfP148xO
-# x3iTerLtGa0EIG6eqYKggdFMbx0NxZazavQZwk36l4jrN5TBS5fngfO3H7koosHz
-# YnDb+gghURXKswI+MYIBxTCCAcECAQEwJDAQMQ4wDAYDVQQDDAVUSVRBTgIQXvib
-# BfOpFbJJxUJ9WcAiEDAJBgUrDgMCGgUAoHgwGAYKKwYBBAGCNwIBDDEKMAigAoAA
-# oQKAADAZBgkqhkiG9w0BCQMxDAYKKwYBBAGCNwIBBDAcBgorBgEEAYI3AgELMQ4w
-# DAYKKwYBBAGCNwIBFTAjBgkqhkiG9w0BCQQxFgQUYw7z9wawDhiH73DpGyFgATw0
-# DL0wDQYJKoZIhvcNAQEBBQAEggEAqfMBHrUeUcMd1OeX9WoIdQUMhgppa+yHZP6x
-# Zz3rwLIBEyAmi6CWU9KdeTl+GU7AWpOnX+ElMcOqwZaGnKnnUd6N0Kotfv/j2Ols
-# a5qjed4CxJLv4TOJQVzEzErEiN+reiKbgtuusWGW41nxc2jOuAxHo2mTlMZml+SS
-# 6Qdz8x4A7o87Aw9BzXAhM3sQzx1AHZsm0RU4klCdxrApuYvvnfim7KGG+V24eaTI
-# vrR8MZuLpQjcrcDHVZnxR0gAEP+8q25fUFgvX3CU/KMDSD4xTujzspe8Tk9lCB0b
-# wysvYgYO3RQLMjf5Gxv/JKj+tRdN9P+8M/rIjDR23W0LsS73Xg==
-# SIG # End signature block
